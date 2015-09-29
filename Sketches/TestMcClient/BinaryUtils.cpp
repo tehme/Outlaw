bb23155431c1207c9b7522bcb7c2372e7030e222
tehme <tehmemc@gmail.com>
2015-09-30 02:00:46 +0400

    TestMcClient: BinaryUtils::dumpHex() fixes.

--- Sketches/TestMcClient/BinaryUtils.cpp
+++ Sketches/TestMcClient/BinaryUtils.cpp
@@ -1,6 +1,7 @@
 #include "BinaryUtils.hpp"
 #include <iostream>
 #include <iomanip>
+#include <algorithm>
 
 //----------------------------------------------------------------------------//
 
@@ -10,20 +11,25 @@ void BinaryUtils::dumpHex(const char * bytes, int size)
     if(size % 16 != 0)
         nRows += 1;
 
-    for(int rowNumber = 0; rowNumber <= nRows; ++rowNumber)
+    for(int rowNumber = 0; rowNumber < nRows; ++rowNumber)
     {
         const int offset = rowNumber * 16;
+        const int rowSize = std::min(size - offset, 16);
 
-        int rowSize = size - offset;
-        if(rowSize > 16)
-            rowSize = 16;
+        if(offset == size)
+        {
+            std::cout << "Ololo!" << std::endl;
+        }
 
         for(int colNumber = 0; colNumber < rowSize; ++colNumber)
         {
             uchar c = uchar(bytes[offset + colNumber]);
             std::cout << std::hex << std::setfill('0') << std::setw(2) << uint(c) << ' ';
         }
-        std::cout.write("   ", 16 - rowSize),
+        if(16 - rowSize > 0)
+        {
+            std::cout << std::setfill(' ') << std::setw((16 - rowSize) * 3) << "";
+        }
 
         std::cout << ' ';
 
@@ -32,7 +38,10 @@ void BinaryUtils::dumpHex(const char * bytes, int size)
             char c = bytes[offset + colNumber];
             std::cout << (isprint(c) ? c : '.');
         }
-        std::cout.write(" ", 16 - rowSize),
+        if(16 - rowSize > 0)
+        {
+            std::cout << std::setfill(' ') << std::setw(16 - rowSize) << "";
+        }
 
         std::cout << std::endl;
     }
